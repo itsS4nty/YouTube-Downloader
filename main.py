d5198829bc3fd0531e637b112aff58981ddfb3b9
itsS4nty <50720295+itsS4nty@users.noreply.github.com>
2020-03-25 13:46:08 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import pytube
+from pytube import YouTube
+import moviepy.editor as mp
+import tkinter
+import tkinter.messagebox
+from tkinter import filedialog
+from tkinter import *
+import os
+# Crear la ventana
+top = tkinter.Tk()
+top.title("YouTube Downloader | Santy Alvarez")
+top.geometry("720x520")
+# Funcion para descargar el video
+def downloadVideo():
+    try:
+        video_url = url_video.get()
+        youtube = pytube.YouTube(video_url)
+        video = youtube.streams.first()
+        folder = folderPath.get()
+        video.download(folder)
+        tkinter.messagebox.showinfo("Video descargado", "Video descargado con exito")
+    except:
+        tkinter.messagebox.showerror("Error en la descarga", "Ha habido un error en la descarga del video")
+
+def downloadAudio():
+    try:
+        video_url = url_video.get()
+        youtube = pytube.YouTube(video_url)
+        video = youtube.streams.first()
+        name = YouTube(video_url)
+        folder = folderPath.get()
+        video.download(folder)
+        os.chdir(folder)
+        mp4name = str(name.title)+".mp4"
+        mp3name = str(name.title)+".mp3"
+        print(mp4name)
+        print(mp3name)
+        clip = mp.VideoFileClip("D:\\VideosDescargados\\Antonio Orozco, KAROL G - Dicen.mp4")
+        print("paso")
+        clip.audio.write_audiofile("D:\\VideosDescargados\\Antonio Orozco, KAROL G - Dicen.mp3")
+        print("paso")
+        tkinter.messagebox.showinfo("Audio descargado", "Audio descargado con exito")
+    except:
+        tkinter.messagebox.showerror("Error en la descarga", "Ha habido un error en la descarga del audio")
+
+def setDirectory():
+    folder_selected = filedialog.askdirectory()
+    folderPath.set(folder_selected)
+
+downloadVid = tkinter.Button(top, text="Descargar video", command = downloadVideo).place(x=200,y=100)
+downloadAud = tkinter.Button(top, text="Descargar audio", command = downloadAudio).place(x=400,y=100)
+folderPath = StringVar()
+saveDirectory = tkinter.Button(top, text="¿Donde guardarlo?", command = setDirectory).place(x=420,y=50)
+tkinter.Label(top, text="URL del video").place(x=200,y=50)
+url_video = tkinter.Entry(top)
+url_video.place(x=280,y=50)
+top.mainloop()
---
+++ test.py
@@ -0,0 +1,11 @@
+# import moviepy.editor as mp
+# clip = mp.VideoFileClip("D:\\VideosDescargados\\1.mp4")
+# clip.audio.write_audiofile("D:\\VideosDescargados\\theaudio.mp3")
+#import tkinter
+#import tkMessageBox
+
+#top = tkinter.Tk()
+#def downloadVideo():
+
+
+#top.mainloop()
